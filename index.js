de043a3a140eb57ae004f8c33f5865432aa08dc8
Deemah Almutairi <163601138+Deemah8@users.noreply.github.com>
2024-09-26 19:03:33 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import HomeScreen from './Home';
-import OrdersScreen from './Orders';
-import FavoritesScreen from './Favorites';
-import MoreScreen from './More';
-
-export { HomeScreen, OrdersScreen, FavoritesScreen, MoreScreen }
